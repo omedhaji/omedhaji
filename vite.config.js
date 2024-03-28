537fd9ccba395fae43de5135d61e537a92d433de
omed Haji <h027313k@student.staffs.ac.uk>
2024-03-28 14:53:39 +0000

    complete

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
@@ -15,5 +18,5 @@ export default defineConfig({
         watch: {
             usePolling: true,
         }
-    },
+    }, 
 });
